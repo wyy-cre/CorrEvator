d8926cd88c19eb3aea7f93be1e2f63941c6f3240
Yuyang Wang <164121941+wyy-cre@users.noreply.github.com>
2026-01-22 10:32:33 +0800

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,1 +1,11 @@
+import time
+import subprocess
 
+
+start = time.time()
+subprocess.run(["python", "pre_process.py"])
+subprocess.run(["python", "get_graph.py"])
+subprocess.run(["python", "train.py"])
+subprocess.run(["python", "get_metrics.py"])
+
+print(f"总时常: {(time.time() - start) / 60} min")
